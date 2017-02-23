98708219b6714dbd787a6c2302bbd292120d96ce
RoyMontoya <roy_a18@hotmail.com>
2017-02-22 22:23:01 -0700

    added click listener

--- viewExperiment/app/src/main/java/com/example/rmontoya/viewexperiment/GroupViewActivity.java
+++ viewExperiment/app/src/main/java/com/example/rmontoya/viewexperiment/GroupViewActivity.java
@@ -2,6 +2,9 @@ package com.example.rmontoya.viewexperiment;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class GroupViewActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class GroupViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xml_mode);
+
+        LinearLayout firstLayout = (LinearLayout) findViewById(R.id.first_layout);
+        firstLayout.setClickable(true);
+        firstLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GroupViewActivity.this, "click Event!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
